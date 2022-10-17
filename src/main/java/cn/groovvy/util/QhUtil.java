e1816b14e500ce013fb8a564fe6f3bdef0814c60
huaan.wang <huan.wang@linkflowtech.com>
2022-10-17 15:34:58 +0800

    add retry template

--- src/main/java/cn/groovvy/util/QhUtil.java
+++ src/main/java/cn/groovvy/util/QhUtil.java
@@ -2,6 +2,7 @@ package cn.groovvy.util;
 
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
+import org.springframework.retry.RetryCallback;
 
 
 /**
@@ -13,10 +14,17 @@ public class QhUtil {
     private static final String API = "https://api.lovelive.tools/api/SweetNothings";
 
     public static String getRandomQh() {
-        HttpResponse response = HttpUtil.createGet(API)
-                .setConnectionTimeout(3000)
-                .setReadTimeout(4000)
-                .execute();
-        return response.body().replaceAll("<br>","\n");
+        try {
+            return RetryUtil.getRetryTemplate().execute((RetryCallback<String, Throwable>) retryContext -> {
+                HttpResponse response = HttpUtil.createGet(API)
+                        .setConnectionTimeout(3000)
+                        .setReadTimeout(4000)
+                        .execute();
+                return response.body().replaceAll("<br>", "\n");
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return null;
     }
 }
--- src/main/java/cn/groovvy/util/WeatherUtil.java
+++ src/main/java/cn/groovvy/util/WeatherUtil.java
@@ -4,9 +4,8 @@ import cn.groovvy.bean.Indices;
 import cn.groovvy.bean.Weather;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
-import cn.hutool.json.JSONUtil;
+import org.springframework.retry.RetryCallback;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -25,12 +24,12 @@ public class WeatherUtil {
     private static final String LOCATION_API = "https://geoapi.qweather.com/v2/city/lookup?key=2b98d397eaae4d7caf53395efb224529&location=%s";
     private static final String INDICES_API = "https://devapi.qweather.com/v7/indices/1d?type=0&key=2b98d397eaae4d7caf53395efb224529&location=%s";
 
-    public static String getLocationId(String city){
+    public static String getLocationId(String city) {
         System.out.println(String.format(LOCATION_API, city));
-        HttpResponse response = HttpUtil.createGet(String.format(LOCATION_API, city))
-                .setConnectionTimeout(3000)
-                .setReadTimeout(4000)
-                .execute();
+//        HttpResponse response = HttpUtil.createGet(String.format(LOCATION_API, city))
+//                .setConnectionTimeout(3000)
+//                .setReadTimeout(4000)
+//                .execute();
 //        return parseObj(parseObj(response.body()).getJSONArray("location").get(0)).getStr("id");
         return "101190104";
     }
@@ -40,10 +39,15 @@ public class WeatherUtil {
             city = "江宁区";
         }
         String locationId = getLocationId(city);
-        HttpResponse response = HttpUtil.createGet(String.format(NOW_WEATHER_API, locationId))
-                .setConnectionTimeout(30000)
-                .setReadTimeout(40000)
-                .execute();
+        HttpResponse response = null;
+        try {
+            response = RetryUtil.getRetryTemplate().execute((RetryCallback<HttpResponse, Throwable>) retryContext -> HttpUtil.createGet(String.format(NOW_WEATHER_API, locationId))
+                    .setConnectionTimeout(30000)
+                    .setReadTimeout(40000)
+                    .execute());
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
         return parseObj(parseObj(response.body()).getJSONObject("now")).getStr("temp");
     }
 
@@ -52,28 +56,34 @@ public class WeatherUtil {
             city = "江宁区";
         }
         String locationId = getLocationId(city);
-        HttpResponse response = HttpUtil.createGet(String.format(WEATHER_API, locationId))
-                .setConnectionTimeout(30000)
-                .setReadTimeout(40000)
-                .execute();
+        HttpResponse response = null;
+        try {
+            response = RetryUtil.getRetryTemplate().execute((RetryCallback<HttpResponse, Throwable>) retryContext -> HttpUtil.createGet(String.format(WEATHER_API, locationId))
+                    .setConnectionTimeout(30000)
+                    .setReadTimeout(40000)
+                    .execute());
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
         return parseObj(parseObj(response.body()).getJSONArray("daily").get(0)).toBean(Weather.class);
     }
 
-    public static Map<String,Indices> getIndices(String city) {
+    public static Map<String, Indices> getIndices(String city) {
         if (city == null) {
             city = "江宁区";
         }
         String locationId = getLocationId(city);
-        HttpResponse response = HttpUtil.createGet(String.format(INDICES_API, locationId))
-                .setConnectionTimeout(30000)
-                .setReadTimeout(40000)
-                .execute();
+        HttpResponse response = null;
+        try {
+            response = RetryUtil.getRetryTemplate().execute((RetryCallback<HttpResponse, Throwable>) retryContext -> HttpUtil.createGet(String.format(INDICES_API, locationId))
+                    .setConnectionTimeout(30000)
+                    .setReadTimeout(40000)
+                    .execute());
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
         List<Indices> daily = parseObj(response.body()).getJSONArray("daily").toList(Indices.class);
         return daily.stream().collect(Collectors.toMap(Indices::getType, Function.identity()));
-    }
-
-    public static void main(String[] args) {
-        getWeatherInfo(null);
     }
 
 }
